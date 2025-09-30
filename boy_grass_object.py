2f33a83e6e3a3cdd1ee3790ce7e2c538816a8b41
duswlswh29 <yeonjin1029@naver.com>
2025-09-30 19:03:26 +0900

    과제 시작

---
+++ boy_grass_object.py
@@ -0,0 +1,98 @@
+from pico2d import *
+import random
+
+
+
+# Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
+    def draw(self):
+        self.image.draw(400,30)
+        pass
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x,self.y = random.randint(100, 700),90
+
+        self.image=load_image('run_animation.png')
+        self.frame=random.randint(0,7)
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,90)
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+        pass
+
+class Zombie:
+    def __init__(self):
+        self.x,self.y=100,170
+        self.frame=0
+        self.image=load_image('zombie.run_animation.png')
+    def update(self):
+        self.frame=(self.frame+1)%10
+        self.x+=5
+    def draw(self):
+        frame_width=self.image.w//10
+        frame_height=self.image.h//10
+        self.image.clip_draw(self.frame*frame_width,0,frame_width,frame_height,self.x,self.y,frame_width//2,frame_height//2)
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def reset_world():
+    global running
+    running = True
+
+    global world
+
+    world=[]
+
+    grass=Grass() #Grass 도장을 이영해서 grass객체 생성
+    world.append(grass)
+    team=[Boy()for _ in range(11)]
+    world+=team
+
+    global boy
+    boy=Boy() # 먼저 찍어냄
+
+    pass
+
+def update_world():
+    world.update()
+    boy.update()
+    pass
+def render_world():
+    clear_canvas()
+    world.draw()
+    boy.draw() #호출하고 클래스에 드로우 정의
+    update_canvas()
+
+    pass
+
+
+
+
+open_canvas()
+reset_world()
+
+while running:
+    handle_events()
+    #게임 로직
+    update_world()
+    #렌더링
+    render_world()
+    delay(0.05)
+close_canvas()
